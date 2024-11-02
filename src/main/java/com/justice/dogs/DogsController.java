77f4ccd4f0a27efb51222c025dbd9d05848a22a0
Justice <floopfloop456@gmail.com>
2024-11-02 12:32:57 +0900

    16th commit

--- src/main/java/com/justice/dogs/DogsController.java
+++ src/main/java/com/justice/dogs/DogsController.java
@@ -6,32 +6,20 @@ import com.justice.dogs.holder.DogsRepo;
 
 import jakarta.validation.Valid;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class DogsController {
 
     @Autowired
     private DogsRepo repo;
-    
-    
-    // a variable to indicate where the upload directory for the images is
-    @Value("${upload.directory}")
-    private String UPLOAD_DIRECTORY;
 
     public DogsController(DogsRepo repo) {
         this.repo = repo;
@@ -94,34 +82,5 @@ public class DogsController {
         .orElseThrow(() -> new DogNotFoundException(id));
         repo.delete(dog);
         return "redirect:/home";
-    }
-
-    @GetMapping("/dogs/upload")
-    public String uploadPicPage() {
-        return "upload-img";
-    }
-    
-    // for uploading images
-    @PostMapping("/dogs/upload")
-    public String uploadPicture(Model model, @RequestParam("image") MultipartFile file) throws IOException {
-        if (file.isEmpty()) {
-            model.addAttribute("msg", "Please upload an image");
-            return "upload-img";
-        }
-
-        // defines the path to save the uploaded images
-        Path filePath = Paths.get(UPLOAD_DIRECTORY, file.getOriginalFilename());
-        
-        try {
-           // writes the file to the specified path
-            Files.write(filePath, file.getBytes()); 
-            model.addAttribute("msg", "Uploaded images: " + file.getOriginalFilename());
-        } catch (IOException e) {
-            System.err.println("Failed to save file: " + e.getMessage());
-            model.addAttribute("msg", "Failed to add image, please try again");
-        }
-
-        return "redirect:/home";
-    }
-
+    }    
 }
