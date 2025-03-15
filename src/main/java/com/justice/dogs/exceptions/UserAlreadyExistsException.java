502db92be288ec7abf767369fe5ebea91a4e2277
Justice <floopfloop456@gmail.com>
2025-03-15 17:01:15 +0900

    32nd commit

--- src/main/java/com/justice/dogs/exceptions/UserAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.justice.dogs.exceptions;
-
-public class UserAlreadyExistsException extends RuntimeException {
-
-    public UserAlreadyExistsException(String string) {
-        super(string);
-    }
-}
